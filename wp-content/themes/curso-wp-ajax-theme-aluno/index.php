3ca075c24e3440ccf2d6f5183834c6620daf7faa
theandersonn <theandersonn@gmail.com>
2018-07-17 15:35:22 -0300

    PHP - Programando detalhes do post #11

--- wp-content/themes/curso-wp-ajax-theme-aluno/index.php
+++ wp-content/themes/curso-wp-ajax-theme-aluno/index.php
@@ -31,9 +31,7 @@
 			<!-- fim ajax load -->
 
 			<!-- lista posts -->
-			<section class="lista-posts" id="lista-posts">
-
-			</section>
+			<section class="lista-posts" id="lista-posts"><!-- conteúdo dinâmico --></section>
 			<!-- fim lista posts -->
 
 		</div>
@@ -49,20 +47,6 @@
 
 <!-- modal -->
 <div class="modal fade" id="detalhes-post">
-	<div class="modal-dialog modal-lg">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h5 class="modal-title">Título do post</h5>
-				<button type="button" class="close" data-dismiss="modal">
-					<span aria-hidden="true">&times;</span>
-				</button>
-			</div>
-			<div class="modal-body">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint distinctio facere aliquid consequatur quidem excepturi neque harum. Sit dolorum itaque commodi, vero officiis, ab. Omnis cum dolorum optio, porro aliquid!</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit id, fugiat, ab, laudantium esse eius ipsum consequuntur atque veniam totam, quos sit? Ducimus vero dignissimos fuga consequatur quibusdam similique esse!</p>
-			</div>
-			<div class="modal-footer"></div>
-		</div>
-	</div>
+	<?php get_template_part( '_parts/detalhes' ); ?>	
 </div>
 <?php get_footer(); ?>
